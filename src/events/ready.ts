870c151b21e5295e775353f18152c52d617474c7
Joffrey <77833567+Wakestufou@users.noreply.github.com>
2023-01-19 16:05:40 +0100

    Ajout d'un status

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,11 +2,24 @@ import { Color } from '../utils/Colors';
 import { Event } from '../structures/Event';
 import Logger from '../utils/Logger';
 import DataBaseCheck from '../utils/DataBaseCheck';
+import { ActivityType } from 'discord.js';
 
 export default new Event('ready', (client) => {
     Logger.info('Bot is online ! ' + client.user?.tag, 'READY', Color.FgGreen);
     client.guilds.cache.forEach((element) => {
         Logger.info(`Check database for ${element.name}`, 'DATABASE');
         DataBaseCheck.checkDataBase(element);
+    });
+
+    // Set the client user's presence
+    client.user.setPresence({
+        activities: [
+            {
+                name: 'vous connaissez ffxiv ???',
+                type: ActivityType.Streaming,
+                url: 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
+            },
+        ],
+        status: 'online',
     });
 });
