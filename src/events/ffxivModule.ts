6ec4ede24ef128be38a132853ef5be1703873aa6
Joffrey <77833567+Wakestufou@users.noreply.github.com>
2023-02-07 12:16:43 +0100

    edrf

--- src/events/ffxivModule.ts
+++ src/events/ffxivModule.ts
@@ -11,9 +11,9 @@ export default new Event('ready', async (client) => {
         // Do something every day at 00:00:00
         const random = Math.floor(Math.random() * 86000) * 1000;
 
-        const hour = random / 3600000;
-        const minute = (random % 3600000) / 60000 + 10;
-        const second = ((random % 3600000) % 60000) / 1000;
+        const hour = Math.floor(random / 3600000);
+        const minute = Math.floor((random % 3600000) / 60000 + 10);
+        const second = Math.floor(((random % 3600000) % 60000) / 1000);
 
         Logger.info(`Wait ${random}ms | ${hour}:${minute}:${second}`, 'FFXIV');
 
